997a4106a2b943b2b1cf205703f973fe0ac20127
Dmitry Uzenkov <dimitry.uzenkov@bonacasa.ch>
2019-12-16 19:36:13 +0300

    Updated: HW #2

--- src/modules/users/components/UserEditForm.vue
+++ src/modules/users/components/UserEditForm.vue
@@ -4,38 +4,47 @@
       <label for="lastName">Фамилия</label>
       <input
         id="lastName"
+        v-model="user.lastName"
         type="text"
         class="form-control"
-        :value="user.lastName"
       />
     </div>
     <div class="form-group">
       <label for="firstName">Имя</label>
       <input
         id="firstName"
+        v-model="user.firstName"
         type="text"
         class="form-control"
-        :value="user.firstName"
       />
     </div>
     <div class="form-group">
       <label for="email">Эл. почта</label>
-      <input id="email" type="email" class="form-control" :value="user.email" />
+      <input
+        id="email"
+        v-model="user.email"
+        type="email"
+        class="form-control"
+      />
     </div>
     <div class="form-group form-check">
       <input
         id="isActive"
+        v-model="user.isActive"
         type="checkbox"
         class="form-check-input"
-        :checked="user.isActive"
       />
       <label class="form-check-label" for="isActive">Активен</label>
     </div>
-    <button type="submit" class="btn btn-primary">Обновить</button>
+    <button type="submit" class="btn btn-primary" @click.prevent="updateUser">
+      Обновить
+    </button>
   </form>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "UserEditForm",
 
@@ -43,6 +52,19 @@ export default {
     user: {
       type: Object,
       required: true
+    }
+  },
+
+  methods: {
+    updateUser() {
+      axios
+        .patch("http://localhost:8000/users/" + this.user.id, this.user)
+        .then(() => {
+          alert("Обновлено");
+        })
+        .catch(() => {
+          alert("Ошибка обновления");
+        });
     }
   }
 };
